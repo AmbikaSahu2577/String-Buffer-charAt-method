8c147aabdcdb59441eb80273880dbcbe1322ddef
Ambika Sahu <90752801+AmbikaSahu2577@users.noreply.github.com>
2022-11-21 12:20:21 +0530

    Add files via upload

---
+++ String_Buffer_charAt_Method.java
@@ -0,0 +1,8 @@
+class String_Buffer_charAt_Method
+{
+	public static void main(String[] args) 
+	{
+		StringBuffer sb = new StringBuffer("java progrraming");
+		System.out.println(sb.charAt(3));
+	}
+}
